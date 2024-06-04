e3bf39d8975c792d6a4f5f7deaef54348942a2d7
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-04 13:25:48 +0200

    increase text size

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -2,8 +2,8 @@ const HomeHero = () => {
     return(
         <>
             <section className='flex flex-col bg-homeHero-image bg-center bg-no-repeat bg-cover text-white justify-center h-lvh p-7 md:px-16 pt-20'>
-                <h1 className='text-5xl font-bold'>What time is it?<br/>Parking time!</h1>
-                <p className="pt-6">Digital parking disc, directly on your mobile.<br/>A winning concept in a new, digitalized format.</p>
+                <h1 className='text-5xl font-bold md:text-6xl'>What time is it?<br/>Parking time!</h1>
+                <p className="text-base pt-6 md:text-xl">Digital parking disc, directly on your mobile.<br/>A winning concept in a new, digitalized format.</p>
             </section>
         </>
     )
