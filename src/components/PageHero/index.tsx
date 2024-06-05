5291e4e914efe53451d69d0add1680c136d52ba0
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-05 12:13:21 +0200

    fix backdrop filter on image

--- src/components/PageHero/index.tsx
+++ src/components/PageHero/index.tsx
@@ -4,8 +4,8 @@ type PageHeroProps = {
 
 const PageHero = ({ bgImage }: PageHeroProps) => {
     return(
-        <section className={`flex flex-col ${bgImage} bg-cover bg-center justify-center h-custom-458 text-white pt-20`}>
-            <div className="p-7 md:px-16">
+        <section className={`flex flex-col ${bgImage} bg-cover bg-center justify-center h-custom-458 text-white pt-20 -z-50`}>
+            <div className="p-7 md:px-16 backdrop-blur-sm  md:w-1/2 h-full">
                 <h1 className="text-5xl font-bold md:text-6xl">What time is it?<br/>Parking time!</h1>
                 <p className="text-base pt-6 md:text-xl">Digital parking disc, directly on your mobile.<br/>A winning concept in a new, digitalized format.</p>
             </div>
