8d6ed39eb2cc763546c6129389c27b1befd2e2ac
Diyako-Qadri <qadri.diyako@gmail.com>
2024-06-05 14:19:25 +0200

    internationalization button

--- src/components/LocaleSwitcher/index.tsx
+++ src/components/LocaleSwitcher/index.tsx
@@ -1,28 +1,36 @@
 'use client';
 
-import { useLocale } from 'next-intl';
 import { useRouter } from 'next/navigation';
+import { useLocale } from 'next-intl';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { ChangeEvent, useTransition } from 'react';
 
 const LocaleSwitcher = () => {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localeActive = useLocale();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const nextLocale = e.target.value;
-    router.replace(`/${nextLocale}`);
-    // startTransition(() => {
-      
-    // });
-    console.log(e.target.value);
+    
+    // Construct the new URL with the next locale
+    const newPathname = `/${nextLocale}${pathname.replace(/^\/[a-z]{2}/, '')}`;
+    const newSearchParams = searchParams.toString();
+    const newUrl = `${newPathname}${newSearchParams ? '?' + newSearchParams : ''}`;
+    
+    startTransition(() => {
+      router.replace(newUrl);
+    });
   };
 
   return (
     <label className="border-2 rounded">
       <p className="sr-only">Change Language</p>
       <select
-      defaultValue={localeActive}
-      disabled={isPending}
+        defaultValue={localeActive}
+        disabled={isPending}
         onChange={onSelectChange}
         className="bg-transparent text-white px-2 py-1"
       >
@@ -32,5 +40,6 @@ const LocaleSwitcher = () => {
     </label>
   );
 };
+
 
 export default LocaleSwitcher;
