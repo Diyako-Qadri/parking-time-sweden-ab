48d9292145c2600ae61ecf8f276dca2a6497a675
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-05 15:27:10 +0200

    position hero actions at bottom

--- src/components/HeroActions/index.tsx
+++ src/components/HeroActions/index.tsx
@@ -1,6 +1,6 @@
 const HeroActions = () => {
     return(
-        <div className="z-100 flex text-white bg-pt-green-transparent text-base/8 flex-wrap py-1">
+        <div className="absolute bottom-0 w-full flex text-white bg-pt-green-transparent text-base/8 flex-wrap py-1">
             <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; Easy to use</p>
             <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 md:justify-center"> &#8226; Free to download</p>
             <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; Avoid fines</p>
--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -3,7 +3,7 @@ import HeroActions from "../HeroActions"
 const HomeHero = () => {
     return(
         <>
-            <section className='flex flex-col bg-homeHero-image bg-center bg-no-repeat bg-cover text-white h-lvh pt-20'>
+            <section className='relative flex flex-col bg-homeHero-image bg-center bg-no-repeat bg-cover text-white h-lvh -z-10'>
                 <div className="p-7 md:px-16">
                     <h1 className="text-5xl font-bold md:text-6xl">What time is it?<br/>Parking time!</h1>
                     <p className="text-base pt-6 md:text-xl">Digital parking disc, directly on your mobile.<br/>A winning concept in a new, digitalized format.</p>
