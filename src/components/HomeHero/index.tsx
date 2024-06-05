2c0c910197e8200f9d9a1d14907ad301f2e2ba7b
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-05 15:34:53 +0200

    pass text in props

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,12 +1,19 @@
 import HeroActions from "../HeroActions"
 
-const HomeHero = () => {
+type HomeHeroProps = {
+    headline1: string;
+    headline2: string;
+    heroText1: string;
+    heroText2: string;
+}
+
+const HomeHero = ({headline1, headline2, heroText1, heroText2}: HomeHeroProps) => {
     return(
         <>
-            <section className='relative flex flex-col bg-homeHero-image bg-center bg-no-repeat bg-cover text-white h-lvh -z-10'>
+            <section className='relative flex flex-col bg-homeHero-image justify-center bg-center bg-no-repeat bg-cover text-white h-lvh -z-10'>
                 <div className="p-7 md:px-16">
-                    <h1 className="text-5xl font-bold md:text-6xl">What time is it?<br/>Parking time!</h1>
-                    <p className="text-base pt-6 md:text-xl">Digital parking disc, directly on your mobile.<br/>A winning concept in a new, digitalized format.</p>
+                    <h1 className="text-5xl font-bold md:text-6xl">{headline1}<br/>{headline2}</h1>
+                    <p className="text-base pt-6 md:text-xl">{heroText1}<br/>{heroText2}</p>
                 </div>
                 <HeroActions/>
             </section>
