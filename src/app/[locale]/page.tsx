825724b5f036d6a919a34a0920d3c414202353ec
Diyako-Qadri <qadri.diyako@gmail.com>
2024-06-07 15:49:51 +0200

    fixed some file structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,15 @@
 import { useTranslations } from 'next-intl';
+import HomeHero from "@/components/HomeHero";
 
-export default function index() {
-  
+export default function Home() {
   return (
     <main>
-   
+        <HomeHero 
+          headline1="What time is it?" 
+          headline2="Parking time!" 
+          heroText1="Digital parking disc, directly on your mobile."
+          heroText2="A winning concept in a new, digitalized format."
+        />
     </main>
   );
 }
