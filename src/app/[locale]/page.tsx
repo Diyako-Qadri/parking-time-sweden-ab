bc0096e88f4b36023a76e987fb7e87e49d6c1068
eri.mari <erimarilin@outlook.com>
2024-06-10 16:19:04 +0200

    start cards

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { useTranslations } from 'next-intl';
-
+import HowItWorks from '@/components/HowItWorksSection';
 export default function index() {
   
   return (
     <main>
-   
+      <HowItWorks/>
     </main>
   );
 }
--- src/components/HowItWorksSection/index.tsx
+++ src/components/HowItWorksSection/index.tsx
@@ -4,44 +4,29 @@ import Parking from "../../../public/images/parking-time-logo.png"
 const HowItWorks = () => {
   return (
     <>
-        <section className="flex flex-col bg-pt-background text-center py-28 px-20">
-        <div className="text-center py-20">
-                <h3>How it works</h3>
-        </div>
-        <div className="flex flex-col md:flex-row bg-pt-background text-center gap-6 py-5 px-6">
-            <div className="w-full ">
-                <Image src={Parking} 
-                alt="Description of image 1" 
-                //sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                priority
-                style={{
-                    width: '100%',
-                    height: 'auto'
-                }}
-                />
+        <section className="flex flex-col bg-pt-background text-center py-28 px-10">
+            <div className="py-10">
+                <p>How does it work</p>
             </div>
-            <div className="w-full">
-                <Image src={Parking} 
-                alt="Description of image 2" 
-                //sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                priority
-                 style={{
-                     width: '100%',
-                     height: 'auto'}}
-                />
+            <div className="">
+            <div className="flex flex-col gap-5">
+                <div className="bg-white w-[320px] h-[360px] justify-center border-solid border-2 rounded-2xl shadow-sm">
+                    <h2>Ange ditt registreringsnummer</h2>
+                    <p>Öppna appen, registreara dig med din bils registreringsnummer</p>
+                </div>
+                <div className="bg-white w-[320px] h-[360px] justify-center border-solid border-2 rounded-2xl shadow-sm">
+                    <h2>Del 2</h2>
+                    <p>gör så här för att dfdsfdfdsfsd</p>
+                </div>
+                <div className="bg-white w-[320px] h-[360px] justify-center border-solid border-2 rounded-2xl shadow-sm">
+                    <h2>del 3</h2>
+                    <p>en massa text som kommer hjär</p>
+                </div>
+                
             </div>
-            <div className="w-full">
-                <Image src={Parking} 
-                alt="Description of image 3" 
-                //sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                priority
-                style={{
-                     width: "100%",
-                     height: "auto"
-                 }}
-                />
             </div>
-        </div>
+            
+
             <div className="text-center p-4 px-0">
                 <button>Button for download </button>
             </div>
