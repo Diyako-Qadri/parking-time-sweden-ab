f261153c10e166f349b5343b69710156a65387da
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-10 16:11:02 +0200

    import new images

--- src/components/PageHero/index.tsx
+++ src/components/PageHero/index.tsx
@@ -1,5 +1,3 @@
-import { useTranslations } from "next-intl"
-
 type PageHeroProps = {
     bgImage: string;
     categoryText: string;
@@ -8,9 +6,10 @@ type PageHeroProps = {
 }
 
 const PageHero = ( {bgImage, categoryText, headline, heroText}:PageHeroProps) => {
+    
     return(
         <section className={`flex relative flex-col ${bgImage} bg-cover bg-center h-custom-458 text-white -z-10 pt-20`}>
-            <div className="flex flex-col justify-end p-7 md:px-16 backdrop-blur-sm h-full md:w-1/2">
+            <div className="flex flex-col justify-end p-7 md:px-16 backdrop-blur-sm h-full md:w-3/5">
                 <p className="relative text-lg font-bold flex items-center pb-4">
                     <span className="w-2 h-2 bg-red-600 rounded-full mr-2"></span>
                     {categoryText}
