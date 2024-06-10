c8d722aad12ccc56489282f720a5adde3c6cba16
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-10 13:59:50 +0200

    fix all hero action translations

--- src/components/HeroActions/index.tsx
+++ src/components/HeroActions/index.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from "next-intl"
+
 const HeroActions = () => {
+    const t = useTranslations('heroActions');
+
     return(
         <div className="absolute bottom-0 w-full flex text-white bg-pt-green-transparent text-base/8 flex-wrap py-1">
-            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; Easy to use</p>
-            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 md:justify-center"> &#8226; Free to download</p>
-            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; Avoid fines</p>
-            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 md:justify-center"> &#8226; Reduce stress</p>
+            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; {t('action1')}</p>
+            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 md:justify-center"> &#8226; {t('action2')}</p>
+            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 justify-center"> &#8226; {t('action3')}</p>
+            <p className="w-1/2 flex pt-3 pb-3 md:w-1/4 md:justify-center"> &#8226; {t('action4')}</p>
         </div>
     )
 }
