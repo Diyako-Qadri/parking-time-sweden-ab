f0e1c6caaba6247faf1370fda34f1f3fe42f3a8d
eri.mari <erimarilin@outlook.com>
2024-06-10 16:31:54 +0200

    fixed styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -36,9 +36,9 @@ const Footer = () => {
     const b = useTranslations('NavItem')
     return (
         <>
-        <footer className="flex md:flex-col justify-center h-80 fixed mt-[100px] w-full text-white bg-pt-primary md:px-16">
-            <div className="flex flex-col justify-between md:flex-row border-b-[1px] h-24 border-white">
-                <div className="flex w-[490px] justify-start">
+        <footer className="flex flex-col h-[776px] md:justify-center md:h-80 fixed mt-[100px] w-full text-white bg-pt-primary md:px-12">
+            <div className="flex m-12  pb-[16px] md:pb-[54px]  flex-col justify-between h-[517px] md:h-[130px] md:flex-row border-b-[1px]  border-white">
+                <div className="flex flex-col h-[334px] md:h-[102px] justify-between  md:flex-row md:w-[490px] md:justify-start">
                     <div className="flex w-[80px]">
                         <a href="/">
                         <Image
@@ -50,15 +50,15 @@ const Footer = () => {
                         /></a>
                     </div> 
                     <div className="w-48 h-26 gap-8"> 
-                        <p className="text-sm">{t("Address")}</p>
+                        <p className="text-[18px] font-semibold pb-2">{t("Address")}</p>
                         <p className="text-sm">Nyköpingsvägen 52 | 61150 </p>
                         <p>NYKÖPING</p>
                     </div>
-                    <div className="w-36">
-                        <p className="text-sm">{t("Contact")}</p>
+                    <div className="w-36 h-[102px]">
+                        <p className="text-[18px] pb-1 font-semibold">{t("Contact")}</p>
                         <a href="mailto:info@example.com" className="hover:underline cursor-pointer">info@example.com</a> | 
                         <a href="tel:+46727224400" className="hover:underline cursor-pointer">+46727224400</a>
-                        <div className="flex flex-row w-[40px] justify-between"> 
+                        <div className="flex mt-4 w-[50px] text-lg flex-row md:w-[40px] justify-between"> 
                            <Link href="https://www.instagram.com/parkingtimesweden" passHref legacyBehavior>
                                 <a target="_blank"> 
                                 <FiInstagram />
@@ -74,11 +74,11 @@ const Footer = () => {
                     </div>
                 </div>
                 {/* menu goes here */}
-                <div className="w-52 flex flex-col justify-start">
+                <div className="w-52 flex mt-10 md:mt-0 flex-col justify-start">
                     <div>
                         <p className="font-bold">{b("Links")}</p>
                     </div>
-                    <div className="flex justify-between ">
+                    <div className=" flex h-[135px] justify-around md:justify-between md:start flex-col md:flex-row">
                        {navItem.map((item, index) => (
                         <Link
                         className={`relative group hover:underline ${path === `/${locale}${item.link}` ? 'active' : ''}`}
@@ -91,9 +91,9 @@ const Footer = () => {
                     </div>
             </div>
             </div>
-            <div className="flex flex-col h-14 justify-end">
-                <div className="flex flex-row justify-between">
-                    <p className="text-sm">&copy; {currentYear}</p>
+            <div className="flex  ml-12 md:ml-0  md:flex-col h-14 md:justify-end md:px-12">
+                <div className="flex flex-col-reverse h-[90px] md:flex-row justify-between">
+                    <p className="text-sm">&copy; {currentYear} Parking Time AB</p>
                     <Link href="#" className="hover:underline">Privacy Policy</Link>
                 </div>
             </div>
