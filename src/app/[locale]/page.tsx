226b1bb514b44677b823884d9f2c91adef208e62
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-10 10:48:54 +0200

    recreate hero

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,11 @@
+import HomeHero from '@/components/HomeHero';
 import { useTranslations } from 'next-intl';
 
 export default function index() {
   
   return (
     <main>
-   
+      <HomeHero />
     </main>
   );
 }
