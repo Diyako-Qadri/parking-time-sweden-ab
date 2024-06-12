62d03f6a9930745ded97b65559880fd3e6b59598
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-12 12:30:21 +0200

    remove z-index

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -8,7 +8,7 @@ const HomeHero = () => {
 
     return(
         <>
-        <section className='relative flex flex-col bg-top h-custom-home bg-homeHero-image justify-center bg-no-repeat top-[80px] bg-cover text-white  -z-10'>
+        <section className='relative flex flex-col bg-top h-custom-home bg-homeHero-image justify-center bg-no-repeat top-[80px] bg-cover text-white '>
             <div className="p-7 md:px-16">
                 <h1 className="text-5xl font-bold md:text-6xl">{t('headline1')}<br/>{t('headline2')}</h1>
                 <p className="text-base pt-6 md:text-xl">{t('heroText1')}<br/>{t('heroText2')}</p>
