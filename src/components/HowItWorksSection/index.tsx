a4f971fc7145c3f6e68d0c2f12f6a236245050e3
eri.mari <erimarilin@outlook.com>
2024-06-16 10:53:07 +0200

    added num 01

--- src/components/HowItWorksSection/index.tsx
+++ src/components/HowItWorksSection/index.tsx
@@ -1,10 +1,88 @@
-import Image from "next/image"
-import Parking from "../../../public/images/parking-time-logo.png"
+import { PiPencilLineFill } from "react-icons/pi";
+import { TbParkingCircle } from "react-icons/tb";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { IoIosArrowForward, IoIosArrowDown} from "react-icons/io";
+
 
 const HowItWorks = () => {
   return (
     <>
-        <section className="flex flex-col bg-pt-background text-center py-28 px-10">
+        <div className="flex md:h-screen mx-auto justify-center items-center py-28 bg-pt-background">
+            <div className="container max-w-10xl px-5">
+                <div className="flex flex-col md:flex-row">
+                    <div className="bg-white relative px-5 py-7 space-y-8 rounded-xl border-solid border-2 shadow-sm">
+                        <div className="relative flex w-16 h-10">
+                            <div className="flex flex-row justify-end">
+                            <PiPencilLineFill className="text-pt-red size-7"/>
+                            </div>
+                        </div>
+                        <div className="flex items-end w-[133px] h-[102px] text-[100px] absolute right-[-20px] top-[-20px] font-extrabold text-slate-200">
+                             01
+                        </div>
+                        <h2 className="text-4xl">Enter your registration number</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                    <div className="flex justify-center items-center pb-7 pt-7">
+                        <IoIosArrowForward className="text-4xl text-pt-red hidden md:inline-block"/>
+                        <IoIosArrowDown className="text-4xl text-pt-red md:hidden" />
+                    </div> 
+                    <div className="bg-white px-5 py-7 space-y-8 rounded-xl border-solid border-2 shadow-sm">
+                        <div className="relative w-16 h-10">
+                            <TbParkingCircle className="text-pt-red size-7"/>
+                        </div>
+                        <h2 className="text-4xl">Find your desired parking location</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                    <div className="flex justify-center items-center pb-7 pt-7">
+                        <IoIosArrowForward className="text-4xl text-pt-red hidden md:inline-block"/>
+                        <IoIosArrowDown className="text-4xl text-pt-red md:hidden" />
+                    </div> 
+                    <div className="bg-white px-5 py-7 space-y-8 rounded-xl border-solid border-2 shadow-sm">
+                        <div className="relative w-16 h-10">
+                            <MdOutlineWatchLater className="text-pt-red size-7"/>
+                        </div>
+                        <h2 className="text-4xl">Start your digital parking disc</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                </div>
+                <div className="flex justify-center md:p-8 md:flex items-center flex-col border-solid border-2">button</div>
+            </div>
+        </div>
+
+
+
+
+        {/* <div className="flex md:h-screen mx-auto justify-center items-center py-28 bg-pt-background">
+            <div className="container max-w-10xl px-5">
+                <div className="grid grid-cols-1 md:grid-cols-5 gap-x-2">
+                    <div className="bg-pt-gray1 px-5 py-7 space-y-8 rounded-xl border-solid border-2 shadow-sm">
+                        <div className="relative w-16 h-12">
+                            <PiPencilLineFill className="text-red-700 size-7"/>
+                        </div>
+                        <h2 className="text-4xl">Enter your registration number</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                     <div className="flex justify-center items-center"><IoIosArrowForward className="text-2xl"/></div> 
+                    <div className="bg-white px-5 py-7 space-y-8 rounded-xl">
+                        <div className="relative w-16 h-12">
+                            <TbParkingCircle className="text-red-700 size-7"/>
+                        </div>
+                        <h2 className="text-4xl">Find your desired parking location</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                     <div className="flex justify-center items-center"><IoIosArrowForward /></div> 
+                    <div className="bg-pt-gray1 px-5 py-7 space-y-8 rounded-xl">
+                        <div className="relative w-16 h-12">
+                            <MdOutlineWatchLater className="text-red-700 size-7"/>
+                        </div>
+                        <h2 className="text-4xl">Start your digital parking disc</h2>
+                        <p className="text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui repudiandae tenetur .</p>
+                    </div>
+                    <div>button</div>
+                </div>
+            </div>
+        </div> */}
+        {/* <section className="flex flex-col bg-pt-background text-center py-28 px-10">
             <div className="py-10">
                 <p>How does it work</p>
             </div>
@@ -30,7 +108,7 @@ const HowItWorks = () => {
             <div className="text-center p-4 px-0">
                 <button>Button for download </button>
             </div>
-        </section>
+        </section> */}
     </>
     )
 }
