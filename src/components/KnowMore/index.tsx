5105d59c57fe7e9a16b86f34f10e3a8b070a803e
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-17 20:18:56 +0200

    implement internationalization and final styling

--- src/components/KnowMore/index.tsx
+++ src/components/KnowMore/index.tsx
@@ -1,11 +1,14 @@
 import { Roboto }  from "next/font/google"
+import { useTranslations } from "next-intl";
 
 const roboto = Roboto({
     subsets: ['latin'],
-    weight: ['400', '700'], 
+    weight: ['100', '400', '700'], 
 });
 
 const KnowMore = () => {
+    const t = useTranslations('knowMore');
+
     return(
         <div className="flex flex-col justify-center items-center text-center bg-pt-primary pt-[79px] text-white md:flex-row md:place-content-evenly md:px-28">
             <div className={`flex flex-col items-center ${roboto.className} md:mx-6`}>
@@ -14,12 +17,15 @@ const KnowMore = () => {
                     className="h-[187px] w-[187px] rounded-full my-6"
                     alt="image of CEO">
                 </img>
-                <p className="text-2xl pb-2">Matilda Öhman, CEO </p>
-                <p className="underline cursor-pointer text-lg md:text-[16px]"><a href="mailto:info@parkingtime.se"></a>info@parkingtime.se</p>
+                <p className="text-2xl pb-2">{t('name-title')}</p>
+                <p className="underline cursor-pointer text-lg md:text-[16px] font-thin">
+                    <a href="mailto:info@parkingtime.se"></a>
+                        {t('mail')}
+                </p>
             </div>
-            <div className="md:text-left md:mx-6">
-                <p className="text-4xl py-6">Want to know more?</p>
-                <p className={`text-lg pb-6 ${roboto.className}`}>text</p>
+            <div className="text-center md:text-left w-[300px] md:mx-6">
+                <p className="text-4xl py-6">{t('question')}</p>
+                <p className={`text-lg pb-6 ${roboto.className} font-thin`}>{t('description')}</p>
             </div>
         </div>
     )
