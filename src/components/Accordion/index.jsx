d48ed8684f47d4513106dc0f186c1ad8d88ab47e
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-17 11:04:08 +0200

    change to arrow symbol

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -6,32 +6,22 @@ const Accordion = ({ question, answer, accordionOpen, onToggle }) => {
     <div className="flex flex-col align-middle px-6 py-4 my-2 rounded-[25px] bg-white border border-pt-lightgrayborder md:max-w-[768px]">
       <div
         onClick={onToggle}
-        className="flex justify-between self-center w-full text-pt-dark-text text-start border-pt-lightgrayborder font-bold text-md cursor-pointer md:text-lg"
+        className="flex justify-between self-center w-full text-pt-dark-text text-start border-pt-lightgrayborder font-bold text-md cursor-pointer perspective-1000 md:text-lg"
         >
         <span>{question}</span>
         <svg
-          className="fill-pt-dark-text shrink-0 ml-8 mt-[5px]"
-          width="16"
-          height="16"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill='none'
           xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "!rotate-180"
-            }`}
+          className={`text-pt-dark-text transform origin-center transition duration-300 ease-out ${
+            accordionOpen && "rotate-180"
+          }`}
+          >
+          <path
+            d="M17.6569 16.2427L19.0711 14.8285L12.0001 7.75739L4.92896 14.8285L6.34317 16.2427L12.0001 10.5858L17.6569 16.2427Z"
+            fill="currentColor"
           />
         </svg>
       </div>
