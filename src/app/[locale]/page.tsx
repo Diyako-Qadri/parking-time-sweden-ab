597e9dbdad2f2cf07b101918898c57b741b106cc
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-06-17 19:44:53 +0200

    create component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import HomeHero from "@/components/HomeHero";
 import WhyParkingTime from "@/components/WhyParkingTime";
 import ThrustedBy from "@/components/ThrustedBy";
+import KnowMore from "@/components/KnowMore";
 import Faq from "@/components/Faq"
 import Statistics from '@/components/Statistics';
 
@@ -11,6 +12,7 @@ export default function index() {
       <WhyParkingTime/>
       <ThrustedBy/>
       <Statistics />
+      <KnowMore />
       <Faq />
     </main>
   );
