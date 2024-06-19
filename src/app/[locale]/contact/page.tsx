c4857e5aa9d7d6280f695ece244726ceea7c1ebb
Diyako-Qadri <qadri.diyako@gmail.com>
2024-06-19 10:41:56 +0200

    fixed contact form for contact page

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,14 +1,28 @@
-import { useTranslations } from "next-intl";
-import PageHero from "@/components/PageHero";
-
+import { useTranslations } from 'next-intl';
+import PageHero from '@/components/PageHero';
+import { ContactUs } from '@/components/ContactForm';
 const contact = () => {
-    const t = useTranslations('contactHero');
+  const t = useTranslations('contactHero');
 
-    return (
+  return (
+    <>
+      
         <div className="hidden md:block">
-            <PageHero bgImage={'bg-contactHero-image'} categoryText={t('categoryText')} headline={t('headline')} heroText={t('heroText')} />
+          <PageHero
+            bgImage={'bg-contactHero-image'}
+            categoryText={t('categoryText')}
+            headline={t('headline')}
+            heroText={t('heroText')}
+          />
         </div>
-    )
+        <div className="p-4">
+          <div>
+            <ContactUs />
+          </div>
+          <div></div>
+        </div>
+    </>
+  );
 };
 
-export default contact
+export default contact;
