14c46b27284ebae60149b29be3349ff37f9e4737
eri.mari <erimarilin@outlook.com>
2024-06-23 22:13:50 +0200

    test

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,13 +1,19 @@
 import { useTranslations } from "next-intl";
 import PageHero from "@/components/PageHero";
-
+import OurValues from "@/components/OurValues";
 const about = () => {
     const t = useTranslations('aboutHero');
 
     return (
-        <div>
+        <>
+         <div>
             <PageHero bgImage={'bg-aboutHero-image'} categoryText={t('categoryText')} headline={t('headline')} heroText={t('heroText')}/>
         </div>
+        <div>
+            <OurValues />
+        </div>
+        </>
+       
     )
 };
 
---
+++ src/components/OurValues/index.tsx
@@ -0,0 +1,18 @@
+const OurValues = () => {
+    return (
+        <>
+        <div className="h-36">
+            <div className="">
+                header + text 2 divs?
+            </div>
+            <div>
+                <div>img + header + text</div>
+                <div>img + header + text</div>
+                <div>img + header + text</div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default OurValues
