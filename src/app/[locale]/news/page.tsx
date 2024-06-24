d4d775b543acc461342e03f74255e85948203738
davidboll <david.boll@me.com>
2024-06-24 11:58:46 +0200

    Adds load more btn to newspage

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -27,6 +27,7 @@ const News = () => {
   const t = useTranslations("newsHero");
   const locale = useLocale();
   const [newsItems, setNewsItems] = useState<NewsItem[]>([]);
+  const [visibleNewsCount, setVisibleNewsCount] = useState(3);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,6 +37,10 @@ const News = () => {
 
     fetchData();
   }, [locale]);
+
+  const loadMoreNews = () => {
+    setVisibleNewsCount((prevCount) => prevCount + 3);
+  };
 
   const baseUrl = "http://localhost:1337";
 
@@ -48,13 +53,13 @@ const News = () => {
         heroText={t("heroText")}
       />
       <section className="bg-pt-background text-center py-16">
-        <h2 className="text-lg font-bold leading-[21.6px]"> {t("news")}</h2>
+        <h2 className="text-lg font-bold leading-[21.6px]">{t("news")}</h2>
         <h3 className="text-4xl font-bold leading-[57.6px] text-center text-[#0B051D] pt-4 pb-9">
           {t("Latest News")}
         </h3>
-        <div className="relative container mx-auto max-w-screen-xl pl-4">
-          <div className="-ml-4 pl-4 flex space-x-6 overflow-x-auto hide-scroll-bar snap-x snap-mandatory pb-8">
-            {newsItems.map((news) => {
+        <div className="relative container mx-auto max-w-screen-xl">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {newsItems.slice(0, visibleNewsCount).map((news) => {
               const imageUrl = news.attributes.Image?.data?.attributes.url
                 ? baseUrl + news.attributes.Image.data.attributes.url
                 : "/path/to/default/image.jpg";
@@ -63,9 +68,7 @@ const News = () => {
               ).join(" ");
 
               return (
-                <div
-                  key={news.id}
-                  className="flex-shrink-0 w-[80%] sm:w-auto snap-start first:pl-4 last:pr-4">
+                <div key={news.id} className="flex-shrink-0 snap-start">
                   <NewsCard
                     image={imageUrl}
                     title={news.attributes.Title}
@@ -77,6 +80,15 @@ const News = () => {
               );
             })}
           </div>
+          {visibleNewsCount < newsItems.length && (
+            <div className="mt-8 flex justify-center">
+              <button
+                onClick={loadMoreNews}
+                className="w-[160px] h-[48px] py-[15px] px-[32px] gap-[8px] rounded-[50px] bg-[#0B051D] text-white shadow-[1px_1px_30px_0px_rgba(0,0,0,0.1)] flex items-center justify-center text-base font-medium leading-[19.2px] text-center">
+                {t("Load More")}
+              </button>
+            </div>
+          )}
         </div>
       </section>
     </>
