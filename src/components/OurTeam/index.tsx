a834f685ad5a18983e2671cb73f1fafd9e5da536
Diyako-Qadri <qadri.diyako@gmail.com>
2024-06-24 13:42:07 +0200

    fixed some styling after feedback from internal testing

--- src/components/OurTeam/index.tsx
+++ src/components/OurTeam/index.tsx
@@ -45,26 +45,27 @@ const OurTeam = () => {
         <h3 className="text-3xl font-bold pb-6">{d('pageTitle')}</h3>
         <p className="text-base font-normal">{d('pageSubtitle')}</p>
       </div>
-      
-        <div className=" relative h-full flex flex-col md:flex-row pt-8 pb-20 gap-12 items-center justify-center flex-wrap ">
-          {ourteam.map((item, index) => (
-            <div className="h-[430px] w-[296px] bg-white rounded-[20px] boxShadow">
-              <div className="relative flex justify-center h-4/5 w-[290px] " key={index}>
-                <Image
-                  src={item.image}
-                  style= {{objectFit:"cover"}}
-                
-                  className="h-[340px]  w-[290px] rounded-t-[20px]"
-                  alt="teammember"
-                ></Image>
-              </div>
-              <div className="flex flex-col justify-between h-1/5 pl-4 p-3">
-                <h3 className="text-xl font-bold">{item.name}</h3>
-                <p className="text-base font-normal">{item.title}</p>
-              </div>
+      <div className=" relative h-full w-full md:w-[70%] xl:w-full flex self-center flex-col md:flex-row pt-8 pb-20 gap-20 items-center justify-center flex-wrap ">
+        {ourteam.map((item, index) => (
+          <div className="h-[430px] w-[296px] md:w-[256px] bg-white rounded-[20px] boxShadow">
+            <div
+              className="relative flex justify-center h-4/5 w-[296px] md:w-[256px] "
+              key={index}
+            >
+              <Image
+                src={item.image}
+                style={{ objectFit: 'cover' }}
+                className="h-[340px] w-[296px]  md:w-[256px] rounded-t-[20px]"
+                alt="teammember"
+              ></Image>
             </div>
-          ))}
-        </div>
+            <div className="flex flex-col justify-between h-1/5 pl-4 p-3">
+              <h3 className="text-xl font-bold">{item.name}</h3>
+              <p className="text-base font-normal">{item.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
