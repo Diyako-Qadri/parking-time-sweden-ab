bd797bdb65c9ada37d79a1fbab1ebf789a21eb06
eri.mari <erimarilin@outlook.com>
2024-06-25 11:33:53 +0200

    styling fix

--- src/components/OurValues/index.tsx
+++ src/components/OurValues/index.tsx
@@ -5,28 +5,30 @@ import { FaUserCheck } from "react-icons/fa";
 const OurValues = () => {
     return (
         <>
-        <div className="flex justify-center w-full h-full">
-            <div className="flex flex-col justify-center text-center">
-                <div className="flex flex-col border border-slate-950">
+        
+        <div className="flex w-full h-full bg-pt-background md:h-[878px]">
+        
+            <div className="flex flex-col items-center justify-center w-full">
+            <div className="block text-center border border-slate-950 bg-pt-background mb-6">  
                     <h2 className="text-3xl font-extrabold">Our Values</h2>
-                    <p className="font-bold">What we believe in</p>
+                    <p className="font-bold">What we believe in</p> 
                 </div>
-                <div className="flex md:flex-row flex-col justify-center ">
+                <div className="flex md:flex-row flex-col md:w-6/12 max-w-full p-8 pt-7"> 
                    
-                    <div className="flex flex-col items-center border border-slate-950 ">
-                        <p className="flex justify-center items-center w-16 h-16 bg-slate-200 border rounded-lg  text-3xl text-pt-red"><FiBox /></p>
-                        <h2>Simplicity</h2>
-                        <p>We are striving to create simple and smooth experiences.</p>
+                    <div className="flex flex-col items-center border border-slate-950 bg-pt-background p-4  md:px-5  md:py-18">
+                        <p className="flex justify-center items-center w-16 h-16 bg-slate-200 border rounded-lg shadow-md text-3xl text-pt-red"><FiBox /></p> 
+                        <h2 className="py-3 font-bold">Simplicity</h2>  
+                        <p className="border text-center">We are striving to create simple and smooth experiences.</p>  
                     </div>
-                    <div className="flex flex-col items-center border border-slate-950">
-                        <p className="flex justify-center items-center w-16 h-16 bg-slate-200 border rounded-lg text-3xl text-pt-red"><HiLightBulb /> </p>
-                        <h2>Innovation</h2>
-                        <p>Always trying to improve and making our apps a leader in its field.</p>
+                    <div className="flex flex-col items-center border border-slate-950 bg-pt-background p-4 md:px-5 md:py-18">
+                        <p className="flex justify-center items-center w-16 h-16 bg-slate-200 border rounded-lg shadow-md text-3xl text-pt-red"><HiLightBulb /> </p>
+                        <h2 className="py-3 font-bold">Innovation</h2>
+                        <p className="text-center">Always trying to improve and making our apps a leader in its field.</p>
                     </div>
-                    <div className="flex flex-col items-center border border-slate-950">
-                        <p className="flex justify-center items-center text-3xl w-16 h-16 bg-slate-200 border rounded-lg text-pt-red"><FaUserCheck /> </p>
-                        <h2>User friendly</h2>
-                        <p>Its crucial that our service meets users needs and enhances their parking experience.</p>
+                    <div className="flex flex-col items-center border border-slate-950 bg-pt-background p-4 md:px-5 md:py-18 ">
+                        <p className="flex justify-center items-center w-16 h-16 bg-slate-200 border rounded-lg shadow-md text-3xl text-pt-red"><FaUserCheck /> </p>
+                        <h2 className="py-3 font-bold">User friendly</h2>
+                        <p className="text-center">Its crucial that our service meets users needs and enhances their parking experience.</p>
                     </div>
                       
                 
